5f8391885aca09d5e7b14512b7dde65c73eba62d
Lili Sazonova <lilissazonova@gmail.com>
2026-04-14 12:06:53 +0300

    fix form

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,27 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const swiperInstance = new Swiper('.mySwiper', {
-        effect: 'slide',
-        navigation: {
-            nextEl: '.custom-button-next',
-            prevEl: '.custom-button-prev',
-        },
-        spaceBetween: 30,
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Swiper(".mySwiper", {
+    modules: [Navigation],
+    effect: "slide",
+    navigation: {
+      nextEl: ".custom-button-next",
+      prevEl: ".custom-button-prev"
+    },
+    spaceBetween: 30,
+    slidesPerView: 3,
+    loop: true,
+    speed: 500,
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      1024: {
         slidesPerView: 3,
-        loop: true,
-        speed: 500,
-        breakpoints: {
-            640: {
-                slidesPerView: 1,
-                spaceBetween: 20,
-            },
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 30,
-            },
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 40,
-            },
-        },
-    });
+        spaceBetween: 40
+      }
+    }
+  });
 });
